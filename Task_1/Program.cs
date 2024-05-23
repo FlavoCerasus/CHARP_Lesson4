ec8618a95abdd4d721ae2d7d4581a73c369f8e97
Danil <eliseev.danii@yandex.ru>
2024-05-23 15:11:08 +0300

    add lesson4:task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Random rand = new Random();
+int [,] createMatrix (int rowCount, int colums)
+{
+int[,] matrix = new int[rowCount, colums];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rand.Next(1, 10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+return matrix;
+}
+void showMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+int [,] matrixTask1 = createMatrix(5,5);
+showMatrix(matrixTask1);

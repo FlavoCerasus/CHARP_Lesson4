3cfce245f46ea1181a42f188365de5eeb047726c
Danil <eliseev.danii@yandex.ru>
2024-05-23 15:12:10 +0300

    add lesson4:task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromString (string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters += e;
+        } 
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(str);
